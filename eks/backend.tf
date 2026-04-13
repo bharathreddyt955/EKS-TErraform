ef0719e3a21124e26a8f6929add7d573ed94a0cf
bharathreddyt955 <bharathreddyt6302@gmail.com>
2026-04-13 06:45:20 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "sample"
+    bucket         = "dev-bha-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
